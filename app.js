739c1ca319d606d17917a87363f33d13c052507f
César Rivera <marbellaspiritmaster@gmail.com>
2024-02-05 16:43:01 -0600

    encriptador completo

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
+let mensajeEncriptado = [];
 let codigo = [['a' , 'ai'],
             ['e' , 'enter'],
             ['i' , 'imes'],
             ['o' , 'ober'],
             ['u' , 'ufat']];
 
-function cortaPalabra(posicion, mensajeCompleto){
+
+function cortaPalabra(posicion, mensajeCompleto, codigo){
     let palabraCortadaIzquierda = [];
     let palabraCortadaDerecha = [];
+    let inicio = 0;
 
-    for (let inicio = 0; inicio < posicion; inicio++) {
+    for (inicio; inicio < posicion; inicio++) {
         palabraCortadaIzquierda = palabraCortadaIzquierda + mensajeCompleto[inicio];        
     }
-    for (posicion+1; posicion < mensajeCompleto.length; posicion++) {
-        palabraCortadaDerecha = palabraCortadaDerecha + mensajeCompleto[posicion];
-    }
-    /*console.log(palabraCortadaIzquierda);
-    console.log(palabraCortadaDerecha);*/
 
+    inicio = inicio+1;
+
+    for (inicio; inicio < mensajeCompleto.length; inicio++) {
+        palabraCortadaDerecha = palabraCortadaDerecha + mensajeCompleto[inicio];
+    }
+
+    mensajeEncriptado = palabraCortadaIzquierda + codigo + palabraCortadaDerecha;
+    return codigo.length;
 }
 
 function encriptar (letra){
-    /*console.log(letra);
-    console.log(posicion);*/
-
     for(let iter=0;iter<codigo.length;iter++){
-        /*console.log(iter);*/
         if(codigo[iter][0]==letra){
             return codigo[iter][1];
         }
@@ -32,25 +34,30 @@ function encriptar (letra){
 }
 
 function capturaTexto(){
-    let mensajeEncriptado = document.querySelector(".msgEncriptado").value;
+    mensajeEncriptado = document.querySelector(".msgEncriptado").value;
     let captura = [];  
-   
-    /*let mensajeEncriptado = document.getElementById('mensajeEnviado').value;   funciona*/
-   
+
     for (let inicio = 0; inicio < mensajeEncriptado.length; inicio++) {
         if(mensajeEncriptado[inicio]=='a'||mensajeEncriptado[inicio]=='e'||mensajeEncriptado[inicio]=='i'||mensajeEncriptado[inicio]=='o'||mensajeEncriptado[inicio]=='u'){
             captura = encriptar(mensajeEncriptado[inicio]);
-            cortaPalabra(inicio,mensajeEncriptado);
-           /* console.log(captura); 
-            console.log(mensajeEncriptado); */
-         }          
+            inicio = inicio + cortaPalabra(inicio, mensajeEncriptado, captura);
+            console.log(mensajeEncriptado); 
+        }          
    }
-/* for (let i = 0; i < codigo.length; i++) {
-        for (let j = 0; j < 2; j++) {
-            console.log(codigo[i][j]);            
-        }
-    }     /*comprobar matriz codigo*/
-    /*console.log(mensajeEncriptado);
-    console.log(codigo);*/
-    
+
+    /*for (let inicio = 0; inicio < mensajeEncriptado.length; inicio++) {
+            if(mensajeEncriptado[inicio]=='a'||mensajeEncriptado[inicio]=='e'||mensajeEncriptado[inicio]=='i'||mensajeEncriptado[inicio]=='o'||mensajeEncriptado[inicio]=='u'){
+                captura = desencriptar(mensajeEncriptado[inicio]);
+            }   
+        }**/
 }
+
+   function desencriptar(letra){
+        for(let iter=0;iter<codigo.length;iter++){
+            if(codigo[iter][0]==letra){
+                return codigo[iter][1];
+            }
+        }
+   }
+
+
