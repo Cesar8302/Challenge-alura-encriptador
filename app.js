db9fa3582aec4b96ef3c15443825c54e939d7032
César Rivera <marbellaspiritmaster@gmail.com>
2024-02-05 15:35:57 -0600

    Cortar palabras

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ let codigo = [['a' , 'ai'],
             ['o' , 'ober'],
             ['u' , 'ufat']];
 
+function cortaPalabra(posicion, mensajeCompleto){
+    let palabraCortadaIzquierda = [];
+    let palabraCortadaDerecha = [];
 
-function encriptar (letra, iter){
-    console.log(letra);
+    for (let inicio = 0; inicio < posicion; inicio++) {
+        palabraCortadaIzquierda = palabraCortadaIzquierda + mensajeCompleto[inicio];        
+    }
+    for (posicion+1; posicion < mensajeCompleto.length; posicion++) {
+        palabraCortadaDerecha = palabraCortadaDerecha + mensajeCompleto[posicion];
+    }
+    /*console.log(palabraCortadaIzquierda);
+    console.log(palabraCortadaDerecha);*/
 
-    for(iter;iter<codigo.length;iter++){
-        console.log(iter);
+}
+
+function encriptar (letra){
+    /*console.log(letra);
+    console.log(posicion);*/
+
+    for(let iter=0;iter<codigo.length;iter++){
+        /*console.log(iter);*/
         if(codigo[iter][0]==letra){
             return codigo[iter][1];
         }
@@ -24,21 +39,17 @@ function capturaTexto(){
    
     for (let inicio = 0; inicio < mensajeEncriptado.length; inicio++) {
         if(mensajeEncriptado[inicio]=='a'||mensajeEncriptado[inicio]=='e'||mensajeEncriptado[inicio]=='i'||mensajeEncriptado[inicio]=='o'||mensajeEncriptado[inicio]=='u'){
-            captura = encriptar(mensajeEncriptado[inicio],0);
-            console.log(captura); 
-            mensaje
-            console.log(mensajeEncriptado); 
+            captura = encriptar(mensajeEncriptado[inicio]);
+            cortaPalabra(inicio,mensajeEncriptado);
+           /* console.log(captura); 
+            console.log(mensajeEncriptado); */
          }          
    }
-   
-    
-
-  /* for (let i = 0; i < codigo.length; i++) {
+/* for (let i = 0; i < codigo.length; i++) {
         for (let j = 0; j < 2; j++) {
             console.log(codigo[i][j]);            
         }
     }     /*comprobar matriz codigo*/
-    
     /*console.log(mensajeEncriptado);
     console.log(codigo);*/
     
