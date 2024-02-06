569bfde32baedb578b277d609ed854fe18411f98
César Rivera <marbellaspiritmaster@gmail.com>
2024-02-05 22:46:45 -0600

    resubido sin cambio

--- app2.js
+++ app2.js
@@ -4,30 +4,27 @@ let codigo = [['a' , 'ai'],
             ['i' , 'imes'],
             ['o' , 'ober'],
             ['u' , 'ufat']];
-
-            
 function cortaPalabra(posicion, mensajeCompleto, codigo){
     let palabraCortadaIzquierda = [];
     let palabraCortadaDerecha = [];
     let inicio = 0;
+    
     for (inicio; inicio < posicion; inicio++) {
         palabraCortadaIzquierda = palabraCortadaIzquierda + mensajeCompleto[inicio];        
-        }
+    }
+
     inicio = inicio+1;
+
     for (inicio; inicio < mensajeCompleto.length; inicio++) {
         palabraCortadaDerecha = palabraCortadaDerecha + mensajeCompleto[inicio];
     }
+
     mensajeEncriptado = palabraCortadaIzquierda + codigo + palabraCortadaDerecha;
-    /*console.log(mensajeEncriptado);*/
     return codigo.length;
 }
 
 function encriptar (letra){
-    /*console.log(letra);
-    console.log(posicion);*/
-
     for(let iter=0;iter<codigo.length;iter++){
-        /*console.log(iter);*/
         if(codigo[iter][0]==letra){
             return codigo[iter][1];
         }
@@ -35,31 +32,27 @@ function encriptar (letra){
 }
 
 function capturaTexto(){
-    let mensajeEncriptado = document.querySelector(".msgEncriptado").value;
     mensajeEncriptado = document.querySelector(".msgEncriptado").value;
     let captura = [];  
-
-    /*let mensajeEncriptado = document.getElementById('mensajeEnviado').value;   funciona*/
-
     for (let inicio = 0; inicio < mensajeEncriptado.length; inicio++) {
         if(mensajeEncriptado[inicio]=='a'||mensajeEncriptado[inicio]=='e'||mensajeEncriptado[inicio]=='i'||mensajeEncriptado[inicio]=='o'||mensajeEncriptado[inicio]=='u'){
             captura = encriptar(mensajeEncriptado[inicio]);
-            cortaPalabra(inicio,mensajeEncriptado);
-           /* console.log(captura); 
-            console.log(mensajeEncriptado); */
             inicio = inicio + cortaPalabra(inicio, mensajeEncriptado, captura);
-           /* console.log(captura); */
-           /* console.log(mensajeEncriptado); */
-         }          
+            console.log(mensajeEncriptado); 
+        }          
    }
-/* for (let i = 0; i < codigo.length; i++) {
-        for (let j = 0; j < 2; j++) {
-            console.log(codigo[i][j]);            
-        }
-    }     /*comprobar matriz codigo*/
-    /*console.log(mensajeEncriptado);
-    console.log(codigo);*/
-    /*console.log(mensajeEncriptado);*/
-    /*console.log(codigo);*/
 
+    /*for (let inicio = 0; inicio < mensajeEncriptado.length; inicio++) {
+            if(mensajeEncriptado[inicio]=='a'||mensajeEncriptado[inicio]=='e'||mensajeEncriptado[inicio]=='i'||mensajeEncriptado[inicio]=='o'||mensajeEncriptado[inicio]=='u'){
+                captura = desencriptar(mensajeEncriptado[inicio]);
+            }   
+        }**/
 }
+
+   function desencriptar(letra){
+        for(let iter=0;iter<codigo.length;iter++){
+            if(codigo[iter][0]==letra){
+                return codigo[iter][1];
+            }
+        }
+   }
