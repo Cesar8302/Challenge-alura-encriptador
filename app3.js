e87e626d3b6ab6d0b15f8f0b10e5d041d5a39496
César Rivera <marbellaspiritmaster@gmail.com>
2024-02-07 23:39:58 -0600

    nuevo metodos encripptado

--- app3.js
+++ app3.js
@@ -10,21 +10,25 @@ let codigoEncriptado = ['ai', 'enter', 'imes', 'ober', 'ufat']
 
 function capturaTexto(){
     mensajeEncriptado = document.querySelector(".msgEncriptado").value;
-    let captura = [];
+    let captura = [''];
+    let seudoCadena = [''];
     
     for (let buscaIndice = 0; buscaIndice < mensajeEncriptado.length; buscaIndice++) {
         if(mensajeEncriptado[buscaIndice]=='a'||mensajeEncriptado[buscaIndice]=='e'||mensajeEncriptado[buscaIndice]=='i'||mensajeEncriptado[buscaIndice]=='o'||mensajeEncriptado[buscaIndice]=='u'){
         captura = queCodigoEs(mensajeEncriptado[buscaIndice]);
         console.log(captura);
-             
+        seudoCadena = seudoCadena + captura;
+        buscaIndice++; 
         }
+        seudoCadena = seudoCadena + mensajeEncriptado [buscaIndice];
+        console.log(seudoCadena);
     }
 }
 
 function queCodigoEs(letra){
     for(let iter=0;iter<codigoDesencriptado.length;iter++){
         if(codigoDesencriptado [iter]==letra){
-            return codigoDesencriptado [iter];
+            return codigoEncriptado [iter];
         }
     }
 }
