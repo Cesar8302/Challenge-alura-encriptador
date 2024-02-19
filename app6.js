bd49c39b15780b9c45e9356ec63e76d888fde90d
César Rivera <marbellaspiritmaster@gmail.com>
2024-02-18 23:46:51 -0600

    tercer

--- app6.js
+++
@@ -1,8 +0,0 @@
-function todoEsMinusculas(campo = document.getElementById("caja-input")) {
-    var regex = /^[a-z \n]+$/; // Expresión regular para verificar si el campo contiene solo letras minúsculas, espacios y saltos de línea
-    if (regex.test(campo.value)) { // Verifica si el valor del campo coincide con la expresión regular
-        return true; // Retorna verdadero si la validación es exitosa
-    } else {
-        return false; // Retorna falso si la validación falla
-    }
-}
